66ff9139dd4072b446520d884c7638c9a9371a5b
amandubey923 <kumaraman19137@gmail.com>
2025-12-22 15:11:45 +0530

    project initiated

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Button> sign in</Button>
+    </div>
+  )
+}
+
+export default page
